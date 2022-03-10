fcf7dc03344d37a0d485f43e259b26711eeece3e
abrhamdan <abrhamdanailrw@yahoo.com>
2022-03-10 10:34:54 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,10 @@
-#!/bin/bash
-gcc $CFILE -o cisfun
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"programming is like building a multilingual puzzle");
+	return (0);
+}
