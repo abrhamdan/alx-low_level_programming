0f85ee7f9aae80ec4550668d5ede3fbc4f7f0c24
abrhamdan <abrhamdanailrw@yahoo.com>
2022-03-10 23:19:39 -0800

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+/*
+ * File: 2-print_alphabet.c
+ * Auth: Brennan DBaraban
+ */
+#include <stdio.h>
+/**
+ * main - Prints the alphabet in lowercase.
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char letter;
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+	putchar('\n);
+	return (0);
+}
