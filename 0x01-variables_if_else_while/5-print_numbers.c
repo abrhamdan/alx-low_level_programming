699191cd6abbd2cf4841073c023f00459e0db789
abrhamdan <abrhamdanailrw@yahoo.com>
2022-03-10 23:51:20 -0800

    five print

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+/*
+ * File: 5-print_numbers.c
+ * Auth: Abrham H Daniel
+ */
+#include <stdio.h>
+/**
+ * main - Prints all single digit numbers of base 10 starting from 0.
+ * Return: Always .
+ */
+int main(void)
+{
+	int num;
+	for (num =0; num < 10; num++)
+		printf("%d", num);
+	printf('\n");
+	return (0);
+}
