b3a61fde022206437de530be5d11b12be10f4b84
abrhamdan <abrhamdanailrw@yahoo.com>
2022-03-14 22:29:44 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * prints 1 0r 0 depending on input
+ * @c: first parameter
+ *
+ * Description: prints all lowercase letters
+ * Return: Always(0).
+ */
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
