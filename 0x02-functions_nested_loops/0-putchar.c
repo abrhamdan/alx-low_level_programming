cbd1d304bc88382a595c277fd7ff3e0945f2844e
abrhamdan <abrhamdanailrw@yahoo.com>
2022-03-15 09:26:24 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include <stdio.h>
 /**
  * main -Entry point of the program
  *
@@ -6,13 +6,6 @@
  */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-	return (0);
+	printf("_putchar\n");
+
+			}
